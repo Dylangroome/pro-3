4413e5a096beda9217a796b6c789aea74bd29029
Dylangroome <dylangroome13@gmail.com>
2022-11-14 06:15:26 +0000

    create row options

--- run.py
+++ run.py
@@ -4,3 +4,9 @@ class tictactoe:
         self.current_winner = None
     def print_board(self):
         for row in [self.board[i*3:(1+1)*3] for i in range(3)]:
+            print('| ' + ' |'.join(row) + ' |')
+    @staticmethod
+    def print_board_nums():
+        number_board = [[strg(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print(('| ' + ' | '.join(row) + ' |'))
