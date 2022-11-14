2cdaac49393c2a0442d27f833b820a07d0d1fc1f
Dylangroome <dylangroome13@gmail.com>
2022-11-14 06:06:43 +0000

    create tictac board

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class tictactoe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
+    def print_board(self):
+        for row in [self.board[i*3:(1+1)*3] for i in range(3)]:
