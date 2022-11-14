8bbe262c14e711657cd0ba220244c61dcc58b077
Dylangroome <dylangroome13@gmail.com>
2022-11-14 06:56:20 +0000

    add make move to play while loop

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ class tictactoe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]
         self.current_winner = None
+
+
     def print_board(self):
         for row in [self.board[i*3:(1+1)*3] for i in range(3)]:
             print('| ' + ' |'.join(row) + ' |')
+
+
     @staticmethod
     def print_board_nums():
         number_board = [[strg(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
@@ -24,6 +28,7 @@ class tictactoe:
         if self.board[square] == ' ':
             self.board[square] = letter
             return True
+        return False
 
 def play(game, x_player, o_player, print_game=True):
     if print_game:
@@ -36,3 +41,9 @@ def play(game, x_player, o_player, print_game=True):
             square = o_player.get_move(game)
         else:
             square = x_player.get_move(game)
+        
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' make a move to square {square}')
+                game.print_board()
+                print('')
