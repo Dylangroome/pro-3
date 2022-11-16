fe2f9f5a671c9662f07f6899ee9866b6ead69e7f
Dylangroome <dylangroome13@gmail.com>
2022-11-16 10:19:05 +0000

    add choice

--- rock.py
+++ rock.py
@@ -4,4 +4,23 @@ import random
 class Game:
 
     def __init__(self):
-        user_input = input('please choose\n rock\n paper\ scissors')
+
+        self.user_input = None
+
+    def get_user_input(self):
+
+        user_input = input('please choose\n \t rock\n \t paper\n \t scissors').strip()
+        if user_input in ['rock']:
+            self.user_input = 'rock'
+        elif user_input in ['paper']:
+            self.user_input = 'paper'
+        elif user_input in ['scissors']:
+            self.user_input = 'scissors'
+        else:
+            print('You didn’t choose a option, please try again.')
+
+    def play(self):
+       
+        self.get_user_input()
+    
+   
