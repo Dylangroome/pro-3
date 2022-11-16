8c5aeb0849ecd10b4e18a411236e5b5b0f30a2c3
Dylangroome <dylangroome13@gmail.com>
2022-11-16 10:01:22 +0000

    add r,p,s game

---
+++ rock.py
@@ -0,0 +1,7 @@
+import random
+
+
+class Game:
+
+    def __init__(self):
+        user_input = input('please choose\n rock\n paper\ scissors')
--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from user import HumanPlayer, RandomComputerPlayer
 
 class tictactoe:
     def __init__(self):
-        self.board = [' ' for _ in range(9)]
-        self.current_winner = None
+        self.board = [' ' for _ in range(9)] # list to rep board 
+        self.current_winner = None # keep track of winner
 
 
     def print_board(self):
@@ -11,7 +11,7 @@ class tictactoe:
             print('| ' + ' |'.join(row) + ' |')
 
 
-    @staticmethod
+    @staticmethod # which number corresponds with each box
     def print_board_nums():
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
@@ -26,7 +26,7 @@ class tictactoe:
     def num_empty_squares(self):
         return self.board.count(' ')
     
-    def make_move(self, square, letter):
+    def make_move(self, square, letter): #if valid move make move
         if self.board[square] == ' ':
             self.board[square] = letter
             if self.winner(square, letter):
@@ -35,19 +35,19 @@ class tictactoe:
         return False
 
     
-    def winner(self, square, letter):
+    def winner(self, square, letter): #check winner
 
-        row_ind = square // 3
+        row_ind = square // 3 # check row
         row = self.board[row_ind*3 : (row_ind + 1) * 3]
         if all([spot == letter for spot in row]):
             return True
         
-        col_ind = square % 3
+        col_ind = square % 3 # check col
         column = [self.board[col_ind+i*3] for i in range(3)]
         if all([spot == letter for spot in column]):
             return True
         
-        if square % 2 == 0:
+        if square % 2 == 0: # check diagonal 
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
             if all([spot == letter for spot in diagonal1]):
                 return True
@@ -61,12 +61,12 @@ class tictactoe:
 
 
 def play(game, x_player, o_player, print_game=True):
-    if print_game:
+    if print_game: # return winner
         game.print_board_nums()
 
     letter = 'X'
 
-    while game.empty_squares():
+    while game.empty_squares(): #iterate while empty square 
         if letter == 'o':
             square = o_player.get_move(game)
         else:
@@ -83,7 +83,7 @@ def play(game, x_player, o_player, print_game=True):
                     print(letter + ' wins!')
                 return letter
 
-            letter = 'o' if letter == 'X' else 'X'
+            letter = 'o' if letter == 'X' else 'X' #alter letter after move
     
     if print_game:
         print('It\'s a tie!')
