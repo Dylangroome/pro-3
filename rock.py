4cff89e3dcde607d900ba5054504d43c786a064c
Dylangroome <dylangroome13@gmail.com>
2022-11-18 11:50:13 +0000

    add win lose option

--- rock.py
+++ rock.py
@@ -3,8 +3,11 @@ import random
 
 class Game:
 
-    def __init__(self):
+    def __init__(self, comp_wins=0, player_wins=0, games_played=0):
 
+        self.comp_wins = comp_wins
+        self.player_wins = player_wins
+        self.games_played = games_played
         self.user_input = None
         self.computer_choose = None
 
@@ -19,40 +22,105 @@ class Game:
             self.user_input = 'scissors'
         else:
             print('You didn’t choose a option, please try again.')
-
+    
 
 
 
     def get_computer_input(self):
-        computer_choose == random.randint(1, 3)
+        computer_choose = random.randint(1, 3)
         if computer_choose == 1:
-            self.computer_choose = 'rock'
+            self.computer_choose ='rock'
         elif computer_choose == 2:
-            self.computer_choose = 'paper'
-        else computer_choose = 'scissors'
-        
+            self.computer_choose ='paper'
+        else:
+            self.computer_choose ='scissors'
+
+    
+    def display_match_results(self):
+        """
+        Displays match results.
+        Adds match results.
+        """
+        print("")
+        print("Player wins: " + str(self.player_wins))
+        print("Computer wins: " + str(self.comp_wins))
+        print("")
+    
+
+    def winner(self):
+        self.games_played += 1
+        if self.user_input == "rock":
+            if self.computer_choose == "rock":
+                print(
+                    "You chose rock. The computer chose rock too. Congrats, you tied.")  
+
+            elif self.computer_choose == "paper":
+                print("You chose rock. The computer chose paper. you lose.")  
+                self.comp_wins += 1
+
+            elif self.computer_choose == "scissors":
+                print("You chose rock. The computer chose scissors. You win!")  
+                self.player_wins += 1
+
+        elif self.user_input == "paper":
+
+            if self.computer_choose == "rock":
+                print(
+                    "You chose paper. The computer chose rock. You win!.")  
+                self.player_wins += 1
+
+            elif self.computer_choose == "paper":
+                print(
+                    "You chose paper. The computer chose paper too. Congrats, you tied.")  
+
+            elif self.computer_choose == "scissors":
+                print(
+                    "You chose paper. The computer chose scissors. you lose.")  
+                self.comp_wins += 1
+
+        elif self.user_input == "scissors":
+
+            if self.computer_choose == "rock":
+                print(
+                    "You chose scissors. The computer chose rock. you lose.")  
+                self.comp_wins += 1
+
+            elif self.computer_choose == "paper":
+                print(
+                    "You chose scissors. The computer chose paper. You win!.")  
+                self.player_wins += 1
+
+            elif self.computer_choose == "scissors":
+                print(
+                   "You chose scissors. The computer chose scissors too. Congrats, you tied.")  
+    
+
 
 
     def play(self):
        
         self.get_user_input()
-
+        self.get_computer_input()
+        self.winner()
+        self.display_match_results()
         
 
 
+        
 def main():
     
     
 
     game = Game()
     
-    
+        
     while True:
 
         
         
         return game.play()
-        
+    
 
+    
 
 main()
